3a735164023a5e158e76d8617ebf7d55281b1c3b
PC <daway4554@gmail.com>
2026-05-31 17:56:26 +0300

    решение

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,19 @@
+import getHealthStatus from "..";
+
+describe("getHealthStatus", () => {
+    it("выводит 'healthy', если health > 50", () => {
+        expect(getHealthStatus({ name: "Маг", health: 90})).toBe("healthy")
+    })
+
+    it("выводит 'wounded', если health = 50", () => {
+        expect(getHealthStatus({ name: "Маг", health: 50})).toBe("wounded")
+    })
+
+    it("выводит 'wounded', если health = 15", () => {
+        expect(getHealthStatus({ name: "Маг", health: 15})).toBe("wounded")
+    })
+
+    it("выводит 'critical', если health < 15", () => {
+        expect(getHealthStatus({ name: "Маг", health: 10})).toBe("critical")
+    })
+})
